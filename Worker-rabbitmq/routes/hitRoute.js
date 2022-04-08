69d6d0e13412035250b762b3805d5aa1fc070ef8
Udit Singh <43987867+udit1707@users.noreply.github.com>
2022-04-08 15:40:10 +0530

    Add files via upload

---
+++ Worker-rabbitmq/routes/hitRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',async(req,res,next)=>{res.status(200).json({message:"Hit Success"})});
+
+
+module.exports=router;
